a7ffc56b71fe99607c6b58a5cf7a28707e495b45
FediiBohdan <gizelbard@ukr.net>
2021-07-16 15:19:50 +0300

    Added exception handlers for wrong customer id

---
+++ src/main/java/ua/fedii/spring/rest/CustomerErrorResponse.java
@@ -0,0 +1,39 @@
+package ua.fedii.spring.rest;
+
+public class CustomerErrorResponse {
+    private int status;
+    private String message;
+    private Long timeStamp;
+
+    public CustomerErrorResponse() {}
+
+    public CustomerErrorResponse(int status, String message, Long timeStamp) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
--- src/main/java/ua/fedii/spring/rest/CustomerRestController.java
+++ src/main/java/ua/fedii/spring/rest/CustomerRestController.java
@@ -2,6 +2,7 @@ package ua.fedii.spring.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.fedii.spring.entity.Customer;
@@ -18,5 +19,16 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+        Customer customer = customerService.getCustomer(customerId);
+
+        if (customer == null) {
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        }
+
+        return customer;
     }
 }
